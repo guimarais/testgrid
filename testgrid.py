54c0c0f692a11448861eaa8c27d308f6d4c3f649
Luis Guimarais <luis.guimarais@volteuropa.org>
2021-08-28 00:14:07 +0100

    Datetime conversion for ease of code

--- testgrid.py
+++ testgrid.py
@@ -3,21 +3,29 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('testdata.txt')
 
+#Defines number os rows and columns
 cols = 3
 rows = 2
 
-fig, ax = plt.subplots(rows, cols)
+fig, ax = plt.subplots(rows, cols)## Maybe add this option: sharex=True)
 
+## Get different device ids
 ids = df['S_DeviceID'].unique()
 idnum = 0
 
+## Sets the indices as datetimes to easy plotting ahead
+df['ts'] = pd.to_datetime(df['ts'])
+df.set_index('ts', inplace=True)
+
+
+# plotting loop
 for c in range(cols):
     for r in range(rows):
         id_to_plot = ids[c + cols*r]
-
-        ax[r, c].plot(df[df['S_DeviceID']==id_to_plot].tr_i, df[df['S_DeviceID']==id_to_plot].tr_u)
-
-
+        
+        df['tr_u'].plot(ax=ax[r, c])
+        df['tr_i'].plot(ax=ax[r, c])
+    
+    
 plt.tight_layout()
 plt.show()
-
